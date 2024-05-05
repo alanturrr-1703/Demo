a29608e549df49ed328bbb1306189d1bb284c9d9
Harshit <hariom.nthakkar@gmail.com>
2024-05-05 06:06:25 +0530

    Some Changes

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const itemsRouter = require('./routes/items');
+
+// Middleware
+app.use(express.json()); // Parse JSON request bodies
+
+// Routes
+app.use('/items', itemsRouter);
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('Welcome to my Node.js/Express application!');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
